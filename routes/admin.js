87bfa74e61355f7d98f69188e1afe0778a1ba776
Akshay Bengani <akshaybengani@gmail.com>
2021-04-10 21:44:28 +0530

    Project Created

---
+++ routes/admin.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const router = express.Router();
+const productsController = require("../controllers/products");
+
+// /admin/add-product => GET
+router.get('/add-product', productsController.getAddProducts);
+
+// /admin/add-product => POST
+router.post('/add-product', productsController.postAddProducts);
+
+module.exports = router;
